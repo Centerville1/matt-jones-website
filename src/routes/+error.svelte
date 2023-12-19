3c3d9eab989ce2ffb66c12bdffb37d50961b5ce1
matthew.jones <matthew.jones@gordon.edu>
2023-12-18 22:46:21 -0500

    Setup initial routing, layout, and styling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<div id="error-page">
+  {#if $page.status == 404}
+    <h2>Oops, silly, that page doesn't exist! ¯\_(ツ)_/¯</h2>
+    <a href="/">Return Home</a>
+  {:else}
+    <h1>{$page.status}</h1>
+    <p>{$page.error?.message}</p>
+  {/if}
+</div>
+
+<style>
+  #error-page {
+    padding-left: 10px;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,80 @@
+<script>
+  // import global styles on all pages.
+  import '../global.css'
+
+  // svelte-media-query docs: https://www.npmjs.com/package/svelte-media-query
+
+</script>
+
+<div id="page">
+  <nav>
+    <div id="logo">
+      logo
+    </div>
+    <div id="links">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/projects">Projects</a>
+    </div>
+  </nav>
+  <main>
+    <div id="page-content">
+      <slot></slot>
+    </div>
+    <footer>
+
+    </footer>
+  </main>
+</div>
+
+<style>
+  #page {
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    background-color: var(--neutral-gray);
+  }
+
+  nav {
+    width: 100vw;
+    height: 60px;
+    background-color: var(--neutral-white);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  #logo {
+    background-color: var(--red);
+    width: 40px;
+    height: 40px;
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white
+  }
+
+  #links {
+    margin-right: 10px;
+
+  }
+
+  main {
+    position: absolute;
+    height: calc(100vh - 60px);
+    width: 100vw;
+    overflow: scroll;
+  }
+
+  #page-content {
+    min-height: 80vh;
+  }
+
+  footer {
+    height: 300px;
+    width: 100%;
+    background-color: var(--neutral-brown);
+  }
+</style>

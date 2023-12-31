7f51607ff15ad8428ead33f318f9449a4f673d31
matthew.jones <matthew.jones@gordon.edu>
2023-12-30 21:33:49 -0500

    Updated error page styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,18 +2,28 @@
   import { page } from '$app/stores';
 </script>
 
-<div id="error-page">
-  {#if $page.status == 404}
-    <h2>Oops, silly, that page doesn't exist! ¯\_(ツ)_/¯</h2>
-    <a href="/">Return Home</a>
-  {:else}
-    <h1>{$page.status}</h1>
-    <p>{$page.error?.message}</p>
-  {/if}
+<div id="page">
+  <div id="error-page">
+    {#if $page.status == 404}
+      <h2>Oops, silly, that page doesn't exist! ¯\_(ツ)_/¯</h2>
+      <a href="/">Return Home</a>
+    {:else}
+      <h1>{$page.status}</h1>
+      <p>{$page.error?.message}</p>
+    {/if}
+  </div>
 </div>
 
 <style>
+  #page {
+    height: 100vh;
+    width: 100vw;
+    background-color: var(--neutral-gray);
+    overflow: hidden;
+  }
+
   #error-page {
     padding-left: 10px;
+    color: var(--contrast-text-light);
   }
 </style>

fbd0bcabde19826cb3d1ccd42cf2f73584c3ee24
matthew.jones <matthew.jones@gordon.edu>
2024-03-06 16:51:52 -0500

    Added vercel analytics tracking code to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
   // import global styles on all pages.
   import '../global.css';
   import ModeSwitcher from './modeSwitcher.svelte';
+  import { inject } from '@vercel/analytics';
+
+  inject();
 </script>
 
 <ModeSwitcher />

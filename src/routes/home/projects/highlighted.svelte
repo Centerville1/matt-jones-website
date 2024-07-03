6c48ff5a5ddd0b185ccbaf8eb9f23dc3e4dcbe82
matthew.jones <matthew.jones@gordon.edu>
2024-07-03 01:25:32 -0400

    Added carousel to highlighted experiences page

--- src/routes/home/projects/highlighted.svelte
+++ src/routes/home/projects/highlighted.svelte
@@ -1,6 +1,14 @@
 <script>
   import experiences from './experiences.json';
   import Card from './card.svelte';
+  import Carousel from 'svelte-carousel';
+  import MediaQuery from 'svelte-media-query';
+
+  let carousel; // for calling methods of the carousel instance
+  
+  const handleNextClick = () => {
+    carousel.goToNext()
+  }
 
   let experienceList = experiences.experiences;
 
@@ -13,41 +21,92 @@
 </script>
 
 <div id="content">
-  <h2>Highlighted Experiences</h2>
-  <div id="cards-scrollbox">
-    <div id="cards">
-      {#if countHighlights > 0}
-        {#each experienceList as experience}
-          {#if experience.highlight}
-            <Card
-              title={experience.title}
-              description={experience.description}
-              linkUrl={experience.url}
-            />
-          {/if}
-        {/each}
-      {:else}
-        <Card
-          title={'No Highlighted Experiences!'}
-          description={'    "Click this card" to navigate to the TIMELINE TAB to see all my projects and positions'}
-          linkUrl={'/home/projects#timeline'}
-        />
-      {/if}
+  <div class="upper-container">
+    <h2>Highlighted Experiences</h2>
+  </div>
+  <div id="upper-content">
+    <div id="carousel-box">
+      <MediaQuery query="(min-width: 700px)" let:matches>
+        {#if matches}
+          <Carousel
+            bind:this={carousel}
+            particlesToShow={2}
+            autoplay={true}
+            autoplayDuration={4000}
+            autoplayProgressVisible={true}
+            duration={1000}
+            pauseOnFocus={true}
+            infinite={true}
+          >
+            {#if countHighlights > 0}
+              {#each experienceList as experience}
+                {#if experience.highlight}
+                  <Card
+                    title={experience.title}
+                    description={experience.description}
+                    linkUrl={experience.url}
+                  />
+                {/if}
+              {/each}
+            {:else}
+              <Card
+                title={'No Highlighted Experiences!'}
+                description={'    "Click this card" to navigate to the TIMELINE TAB to see all my projects and positions'}
+                linkUrl={'/home/projects#timeline'}
+              />
+            {/if}
+          </Carousel>
+        {:else}
+          <Carousel
+            bind:this={carousel}
+            particlesToShow={1}
+            autoplay={true}
+            autoplayDuration={4000}
+            autoplayProgressVisible={true}
+            duration={1000}
+            pauseOnFocus={true}
+            infinite={true}
+          >
+            {#if countHighlights > 0}
+              {#each experienceList as experience}
+                {#if experience.highlight}
+                  <Card
+                    title={experience.title}
+                    description={experience.description}
+                    linkUrl={experience.url}
+                  />
+                {/if}
+              {/each}
+            {:else}
+              <Card
+                title={'No Highlighted Experiences!'}
+                description={'    "Click this card" to navigate to the TIMELINE TAB to see all my projects and positions'}
+                linkUrl={'/home/projects#timeline'}
+              />
+            {/if}
+          </Carousel>
+        {/if}
+        </MediaQuery>
     </div>
   </div>
 </div>
 
 <style>
-  #content {
-    width: 98%;
-    margin-left: auto;
-    margin-right: auto;
+
+  .upper-container {
+    padding-left: 20px;
   }
 
-  #cards-scrollbox {
-    width: 100%;
-    padding-bottom: 20px;
-    overflow-x: scroll;
+  #upper-content {
+    border-top: 6px solid var(--main-blue-light);
+    border-bottom: 6px solid var(--main-blue-light);
+  }
+
+  #carousel-box {
+    width:95%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 20px;
   }
 
   #cards {

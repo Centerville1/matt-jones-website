002a4500f2b74d9bf48f5ae1905508f6b2130feb
matthew.jones <matthew.jones@gordon.edu>
2024-07-16 22:41:38 -0400

    Added cube background to sphere page

--- src/routes/home/sphere/+page.svelte
+++ src/routes/home/sphere/+page.svelte
@@ -15,6 +15,14 @@
     min-height: 80vh;
     width: 100%;
     overflow: hidden;
+    background-image:  linear-gradient(30deg, var(--neutral-dark-gray-op-50) 12%, transparent 12.5%, transparent 87%, var(--neutral-dark-gray-op-50) 87.5%, var(--neutral-dark-gray-op-50)), 
+    linear-gradient(150deg, var(--neutral-dark-gray-op-50) 12%, transparent 12.5%, transparent 87%, var(--neutral-dark-gray-op-50) 87.5%, var(--neutral-dark-gray-op-50)), 
+    linear-gradient(30deg, var(--neutral-dark-gray-op-50) 12%, transparent 12.5%, transparent 87%, var(--neutral-dark-gray-op-50) 87.5%, var(--neutral-dark-gray-op-50)), 
+    linear-gradient(150deg, var(--neutral-dark-gray-op-50) 12%, transparent 12.5%, transparent 87%, var(--neutral-dark-gray-op-50) 87.5%, var(--neutral-dark-gray-op-50)), 
+    linear-gradient(60deg, var(--neutral-dark-gray-op-10) 25%, transparent 25.5%, transparent 75%, var(--neutral-dark-gray-op-10) 75%, var(--neutral-dark-gray-op-10)), 
+    linear-gradient(60deg, var(--neutral-dark-gray-op-10) 25%, transparent 25.5%, transparent 75%, var(--neutral-dark-gray-op-10) 75%, var(--neutral-dark-gray-op-10));
+    background-size: 80px 140px;
+    background-position: 0 0, 0 0, 40px 70px, 40px 70px, 0 0, 40px 70px;
   }
 
   #sphere-container {
